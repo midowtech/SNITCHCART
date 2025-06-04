ef6bd2c0012d3a6a0bc5d69563c9e15edd94bb6f
Abhishek <creater920930@gmail.com>
2025-06-04 16:37:32 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,22 +1,20 @@
 // firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-QxJDh8QU2aI8c2kpfTZBNemyvmjq1LE",
   authDomain: "snitchmart-a8073.firebaseapp.com",
   projectId: "snitchmart-a8073",
-  storageBucket: "snitchmart-a8073.firebasestorage.app",
+  storageBucket: "snitchmart-a8073.appspot.com",
   messagingSenderId: "381112072944",
   appId: "1:381112072944:web:36f7e3269a847a942da035",
   measurementId: "G-Y4LS8V8SLL"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { auth, db };
+export { db, auth };
