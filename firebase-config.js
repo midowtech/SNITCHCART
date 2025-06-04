84509a8aef8d1f0310d45e27fb6b560fdbc94008
Abhishek <creater920930@gmail.com>
2025-06-04 19:37:41 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,6 +0,0 @@
-// firebase-config.js import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-const firebaseConfig = { apiKey: "AIzaSyD-QxJDh8QU2aI8c2kpfTZBNemyvmjq1LE", authDomain: "snitchmart-a8073.firebaseapp.com", projectId: "snitchmart-a8073", storageBucket: "snitchmart-a8073.appspot.com", messagingSenderId: "381112072944", appId: "1:381112072944:web:36f7e3269a847a942da035", measurementId: "G-Y4LS8V8SLL" };
-
-export const app = initializeApp(firebaseConfig);
-
