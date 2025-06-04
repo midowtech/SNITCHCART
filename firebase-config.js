2586c5c9d24f81454a474dd15986f78b3c899908
Abhishek <creater920930@gmail.com>
2025-06-04 20:46:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
@@ -7,13 +7,14 @@ const firebaseConfig = {
   apiKey: "AIzaSyD-QxJDh8QU2aI8c2kpfTZBNemyvmjq1LE",
   authDomain: "snitchmart-a8073.firebaseapp.com",
   projectId: "snitchmart-a8073",
-  storageBucket: "snitchmart-a8073.appspot.com",
+  storageBucket: "snitchmart-a8073.firebasestorage.app",
   messagingSenderId: "381112072944",
   appId: "1:381112072944:web:36f7e3269a847a942da035",
   measurementId: "G-Y4LS8V8SLL"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
